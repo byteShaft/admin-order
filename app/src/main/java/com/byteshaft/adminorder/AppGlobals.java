ea375842690a9ce631dfab1427ac1ac26b0deb63
s9iper1 <s9iper1@gmail.com>
2015-10-20 11:15:28 +0500

    first commit

---
+++ app/src/main/java/com/byteshaft/adminorder/AppGlobals.java
@@ -0,0 +1,36 @@
+package com.byteshaft.adminorder;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.parse.Parse;
+import com.parse.ParseInstallation;
+
+
+public class AppGlobals extends Application {
+
+    private static Context sContext;
+    public static final String APP_ID = "EieYzwuvJCqr2xFlwy8n7MAuofRj4LHa1F1MCudO";
+    public static final String CLIENT_ID = "9sIi4bCDTLYXzd5y0JG5pBaAwaqzJVzlZnfiTvdk";
+    public static final int NOTIFICATION_ID = 2112;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sContext = getApplicationContext();
+        Parse.initialize(this, APP_ID , CLIENT_ID);
+//        ParseInstallation.getCurrentInstallation().saveInBackground();
+        ParseInstallation installation = ParseInstallation.getCurrentInstallation();
+        installation.put("admin", "admin_order_receiver");
+        installation.saveInBackground();
+    }
+
+    public static Context getContext() {
+        return sContext;
+    }
+
+    public static String getLogTag(Class aClass) {
+        return  aClass.getName();
+
+    }
+}
