22ce340c919317ad38046ec567b07d0317cf1c63
husnain446 <husnain446@gmail.com>
2015-10-22 14:09:51 +0500

    reconstruct drawer

--- app/src/main/java/com/byteshaft/adminorder/MainActivity.java
+++ app/src/main/java/com/byteshaft/adminorder/MainActivity.java
@@ -3,6 +3,8 @@ package com.byteshaft.adminorder;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -18,16 +20,18 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.byteshaft.adminorder.database.DatabaseHelpers;
+import com.byteshaft.adminorder.fragments.DeliveredFragments;
+import com.byteshaft.adminorder.fragments.OrderFragment;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
 
     private ArrayList<String> ordersPhoneNumber;
     private ListView mListView;
     private DatabaseHelpers mDatabaseHelpers;
     private ArrayAdapter<String> mArrayAdapter;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,14 +41,58 @@ public class MainActivity extends AppCompatActivity
         mDatabaseHelpers = new DatabaseHelpers(AppGlobals.getContext());
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        setupDrawerContent(navigationView);
     }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        Fragment fragment = null;
+
+        Class fragmentClass;
+        switch (menuItem.getItemId()) {
+            case R.id.orders_fragment:
+                fragmentClass = DeliveredFragments.class;
+                break;
+            case R.id.deliver_fragment:
+                fragmentClass = OrderFragment.class;
+                break;
+            default:
+                fragmentClass = DeliveredFragments.class;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.container, fragment).commit();
+
+        // Highlight the selected item, update the title, and close the drawer
+        menuItem.setChecked(true);
+        setTitle(menuItem.getTitle());
+        drawer.closeDrawers();
+
+    }
+
 
     @Override
     protected void onResume() {
@@ -59,9 +107,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
@@ -71,41 +117,6 @@ public class MainActivity extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.orders) {
-//            startActivity(new Intent(getApplicationContext(), MainActivity.class));
-        } else if (id == R.id.deliver) {
-//            getSupportFragmentManager().beginTransaction()
-//                    .add(R.id.container, new DeliveredFragments())
-//                    .commit();
-        }  else if (id == R.id.nav_share) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 
@@ -139,7 +150,4 @@ public class MainActivity extends AppCompatActivity
         public TextView number;
         public TextView deliveryTime;
     }
-
-
-
 }
